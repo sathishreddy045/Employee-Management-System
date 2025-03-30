d68b2219e2307e6f93bd7c8131f72d2e71ccab9d
Sathish Reddy <sathishreddy045@gmail.com>
2025-03-30 13:23:36 +0530

    Initial commit

---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className="navbar-brand ms-3" href="https://sathishreddy.vercel.app">
+                    Employee Management System
+                </a>
+            </nav>
+        </header>
+    </div>
+  )
+}
